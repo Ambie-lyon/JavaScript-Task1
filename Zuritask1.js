756879e9c156fcbf564dbeeded0794a3ec5cb9f0
Amblessed <105244167+Ambie-lyon@users.noreply.github.com>
2022-06-24 22:39:37 +0100

    Create Zuritask1.js

---
+++ Zuritask1.js
@@ -0,0 +1,11 @@
+alert('My name is Amblessed');
+alert('5.5ft');
+alert('Nigeria');
+
+var name = ('My name is Amblessed Chidilim');
+var height = ('5.5ft');
+var country = ('Nigeria');
+
+console.log(name);
+console.log(height);
+console.log(country);
